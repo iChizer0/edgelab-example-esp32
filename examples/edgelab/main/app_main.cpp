ec999a97c83fc5da67276f59b790557227b14403
nullptr <nullptr@localhost>
2023-07-31 16:48:20 +0800

    wip

---
+++ examples/edgelab/main/app_main.cpp
@@ -0,0 +1,20 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <stdio.h>
+
+#include "edgelab.h"
+
+extern "C" void app_main(void) {
+    ModelLoader model_loader;
+    auto        models{model_loader.get_models()};
+
+    printf("found modles: %d\n", models.size());
+
+    for (int i = 1000; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
+}
